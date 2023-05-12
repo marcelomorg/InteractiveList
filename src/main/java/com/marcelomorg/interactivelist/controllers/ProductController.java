6c74e5cd89ad4a470ba00066ac7b1ecf43319f63
Marcelo Morgado <marceloemail21@gmail.com>
2023-05-11 22:34:06 -0300

    Modified ProductController ProductService and created ProductDTO

--- src/main/java/com/marcelomorg/interactivelist/controllers/ProductController.java
+++ src/main/java/com/marcelomorg/interactivelist/controllers/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcelomorg.interactivelist.dtos.ProductDTO;
 import com.marcelomorg.interactivelist.dtos.ProductMinDTO;
 import com.marcelomorg.interactivelist.services.ProductService;
 
@@ -18,7 +20,18 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<ProductMinDTO> findAll(){
+    public List<ProductDTO> findAll(){
         return productService.findAll();
     }
+
+    @GetMapping("/min")
+    public List<ProductMinDTO> findMinAll(){
+        return productService.findMinAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return productService.findById(id);
+    }
+
 }
--- src/main/java/com/marcelomorg/interactivelist/services/ProductService.java
+++ src/main/java/com/marcelomorg/interactivelist/services/ProductService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.marcelomorg.interactivelist.dtos.ProductDTO;
 import com.marcelomorg.interactivelist.dtos.ProductMinDTO;
 import com.marcelomorg.interactivelist.entities.Product;
 import com.marcelomorg.interactivelist.repositories.ProductRepository;
@@ -14,10 +16,40 @@ public class ProductService {
     
     @Autowired
     private ProductRepository productRepository;
-
-    public List<ProductMinDTO> findAll(){
+    
+    @Transactional(readOnly = true)
+    public List<ProductMinDTO> findMinAll(){
         List<Product> result = productRepository.findAll();
-        List<ProductMinDTO> dtoResult = result.stream().map(x -> new ProductMinDTO(x)).toList();
-        return dtoResult;        
+        List<ProductMinDTO> dtoMinResult = result.stream().map(x -> new ProductMinDTO(x)).toList();
+        return dtoMinResult;        
     }
+
+    @Transactional(readOnly = true)
+    public List<ProductDTO> findAll(){
+        List<Product> result = productRepository.findAll();
+        List<ProductDTO> dtoResult = result.stream().map(item -> new ProductDTO(item)).toList();
+        return dtoResult;
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){        
+        
+        try{
+            ProductDTO idResult = new ProductDTO(productRepository.findById(id).get());
+            return idResult;
+        } catch (Exception e){
+            ProductDTO erro = new ProductDTO();
+            erro.setId(id);
+            erro.setGenre("Not found");
+            erro.setImgUrl("Not found");
+            erro.setLongDescription("Not found");
+            erro.setPlatform("Not Found");
+            erro.setShortDescription("Not Found");
+            erro.setTitle("Not found");
+            System.out.println("Erro at the search in line 39 of ProductService.findybyId method. ERRO: " + e.getMessage());
+            return erro;
+        }
+
+    }
+
 }
